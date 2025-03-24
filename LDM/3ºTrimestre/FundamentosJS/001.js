3ea98bbe77c8a3fcfe35a31479c60f532dba5f2e
AngelRagel05 <jimenezragelangel@gmail.com>
2025-03-24 10:35:01 +0100

    creo las carpetas de los trimestres y muevo todo

---
+++ LDM/3ºTrimestre/FundamentosJS/001.js
@@ -0,0 +1,12 @@
+// 001variables.js/.html: Declara dos variables: admin y name.
+
+// Asigna el valor "John Snow" a name.
+
+// Copia el valor de name a admin.
+
+// Muestra el valor de admin usando alert (debe salir “John Snow”).
+
+let admin;
+let name = "John Snow";
+
+name = admin;
