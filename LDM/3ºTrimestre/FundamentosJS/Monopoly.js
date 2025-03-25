19b47efe572d8abe0d35a26d8d27de4d5efbab96
AngelRagel05 <jimenezragelangel@gmail.com>
2025-03-25 11:35:39 +0100

    subo avance de Monopoly

--- LDM/3ºTrimestre/FundamentosJS/Monopoly.js
+++ LDM/3ºTrimestre/FundamentosJS/Monopoly.js
@@ -1,11 +1,16 @@
 "use strict"
 
 let tickets = [500, 200, 100, 50, 20, 10, 5, 2, 1];
+let usedTickets = [0, 0, 0, 0, 0, 0, 0, 0, 0,];
 
 let amount = 999;
+let i=0;
 
 while(amount > 0) {
-    for(int = 0; i < tickets.length; i++) {
-        
-    }
+    let quotient = amount/tickets[i];
+    (amount % tickets[i] == 0) ? usedTickets[i] = quotient : usedTickets[i] = 0;
+    amount -= tickets[i] * (quotient);
+    i++;
 }
+
+console.log()
