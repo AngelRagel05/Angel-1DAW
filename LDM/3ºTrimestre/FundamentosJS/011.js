aefa204321646d9cdefe462eff7d344389ecd891
AngelRagel05 <jimenezragelangel@gmail.com>
2025-03-25 11:10:54 +0100

    subo dos más y creo "Monopoly"

---
+++ LDM/3ºTrimestre/FundamentosJS/011.js
@@ -0,0 +1,11 @@
+// 011ifStringCero.js/.html: ¿Se mostrará el alert?
+
+if ("0") {
+
+ alert( '¿me ejecuto o no?' );
+
+}
+
+// Diría que no, no hay condición que cumplir
+
+// Pues si, se ejecuta
---
+++ LDM/3ºTrimestre/FundamentosJS/012.js
@@ -0,0 +1,5 @@
+// 012nombreJS.js/.html: Usando el constructor if..else, escribe el código que pregunta: ‘¿Cuál es el nombre “oficial” de JavaScript?’ Si el visitante escribe “ECMAScript”, 
+// entonces muestra: “¡Correcto!”, de lo contrario muestra: “¿No lo sabes? ¡ECMAScript!”
+
+let res= (prompt ("¿Cuál es el nombre 'oficial' de JavaScript?") == "ECMAScript")? console.log("correcto") : console.log( "¿No lo sabes?"); 
+
