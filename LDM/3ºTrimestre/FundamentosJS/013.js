c9d1bc8ca093101366f936e7fbdbf9ac2e7a6cb5
AngelRagel05 <jimenezragelangel@gmail.com>
2025-03-25 13:55:37 +0100

    acabo el 013

--- LDM/3ºTrimestre/FundamentosJS/013.js
+++ LDM/3ºTrimestre/FundamentosJS/013.js
@@ -5,3 +5,6 @@
 // 0, si es igual a cero.
 // En la tarea asumimos que siempre el usuario introduce un número.
 
+let answer = prompt("Introduce un número."); 
+
+answer == "1" ? alert("El número elegido es: 1") : answer == "-1" ? alert("El número elegido es: -1") : answer == "0" ? alert("El número elegido es: 0") : alert("El número introducido no es uno de los elegidos");
