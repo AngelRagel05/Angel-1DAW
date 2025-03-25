6db8873c94a9d0f4994a502a19a809d2b77d29e8
AngelRagel05 <jimenezragelangel@gmail.com>
2025-03-25 10:11:26 +0100

    subo dos ejercicios más

---
+++ LDM/3ºTrimestre/FundamentosJS/010.js
@@ -0,0 +1,15 @@
+// 010comparaciones.js/.html: ¿Cuál será el resultado de las siguientes expresiones?
+
+5 > 4 // True
+
+"apple" > "microsoft" //False
+
+"2" > "12" // True
+
+undefined == null // True
+
+undefined === null // False
+
+null == "\n0\n" // False
+
+null === +"\n0\n" // False
