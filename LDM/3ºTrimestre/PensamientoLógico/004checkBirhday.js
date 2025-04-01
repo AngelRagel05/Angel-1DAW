b0139bb8421c7567739dbecf9d85c689a4222f49
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-01 18:43:05 +0200

    subo el avance del 4

---
+++ LDM/3ºTrimestre/PensamientoLógico/004checkBirhday.js
@@ -0,0 +1,7 @@
+// Crear un programa que pida adivinar tu cumpleaños ingresando por separado ingresar
+// día y mes, en números, y mostrar luego si un mensaje si ha adivinado o no, cada parte,
+// preguntar al usuario si quiere seguir jugando, darle 10 vidas 💚 que vaya perdiendo a
+// medida que haga intentos. Y un mensaje al final de: //Has ganado 🏅!! o uno de: // Has perdido 😿 !!!
+
+"use strict"
+
