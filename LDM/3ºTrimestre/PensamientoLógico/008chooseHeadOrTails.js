2b388bbfd32b76e732c05e058ff5df85aae09fb5
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-07 18:51:54 +0200

    Implementa la lógica del juego de cara o cruz con selección aleatoria y manejo de entradas del usuario

--- LDM/3ºTrimestre/PensamientoLógico/008chooseHeadOrTails.js
+++ LDM/3ºTrimestre/PensamientoLógico/008chooseHeadOrTails.js
@@ -8,3 +8,38 @@
 // SALIR (🚪), en vez de CARA o CRUZ, el programa debe mostrar la cantidad máxima de rondas seguidas acertadas junto a un mensaje de despedida, 
 // y terminar su ejecución.
 
+let selected = "";
+let check = false;
+
+while (!check) {
+    selected = prompt("Introduce cara o cruz. Escriba SALIR para salir del programa.").toLocaleLowerCase();
+    switch (selected) {
+        case "cara":
+        case "cruz":
+            correctChoose(selected, randomChoose());
+            break;
+        case "salir":
+            check = true;
+            break;
+
+        default:
+            alert("Opción no válida.");
+            break;
+    }
+}
+
+function correctChoose(selected, opcion) {
+    let correct = 0;
+    if (selected === opcion) {
+        correct++;
+        console.log(correct);
+
+    } else {
+        console.log("Has fallado.");
+    }
+}
+
+function randomChoose() {
+    let opcion = Math.random() < 0.5 ? "cara" : "cruz";
+    return opcion;
+}
