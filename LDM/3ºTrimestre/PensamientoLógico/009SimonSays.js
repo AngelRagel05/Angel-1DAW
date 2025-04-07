b12bf0f1a976718c1f02a25f337eb0f07e8c5ddd
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-07 19:01:11 +0200

    Añade la implementación inicial del juego "Simón dice" con descripción de la lógica y reglas del juego.

---
+++ LDM/3ºTrimestre/PensamientoLógico/009SimonSays.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// Replicar el juego de "Simón dice". El programa debe mostrar una secuencia de colores, agregando por cada ronda que pase otro color más. 
+// El usuario debe ingresar los colores de la secuencia uno por uno. Si erra la secuencia (es decir, si ingresa algún color mal), el programa 
+// debe terminar. Los colores posibles son: rojo🔴, azul🔵, verde 🟢y amarillo🟡. Hay dos variantes posibles: o el juego continúa indefinidamente 
+// hasta que se erra la secuencia, y muestra cuántas rondas se sobrevivió, o el programa permite elegir al principio cuántas rondas se desea hacer, 
+// y mostrar un mensaje de victoria si se llega a esa cantidad de rondas sin errarle a la secuencia.
+
