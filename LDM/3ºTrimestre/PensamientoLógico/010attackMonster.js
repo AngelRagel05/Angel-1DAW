29e01725efd2390c690c2d3c31dee9eb3cfb625a
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-08 11:56:44 +0200

    Mejora la estructura de la función runGame y añade la función showOption.

--- LDM/3ºTrimestre/PensamientoLógico/010attackMonster.js
+++ LDM/3ºTrimestre/PensamientoLógico/010attackMonster.js
@@ -36,8 +36,30 @@ const MAX_HEALING_POTION = 30;
 
 let opc = 0;
 
-function runGame () {
+function runGame() {
     switch (opc) {
+        case 1:
 
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        default:
+            
+            break;
     }
+}
+
+function showOption() {
+
 }
