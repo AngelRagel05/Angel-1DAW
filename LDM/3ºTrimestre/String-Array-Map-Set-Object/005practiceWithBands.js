d8fb1bd479c0baa20ccc472941691c42062d8670
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-10 20:30:24 +0200

    Reorganiza el código para mejorar la legibilidad y añade comentarios explicativos sobre la búsqueda y filtrado de grupos musicales.

--- LDM/3ºTrimestre/String-Array-Map-Set-Object/005practiceWithBands.js
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/005practiceWithBands.js
@@ -8,7 +8,22 @@
    {nombre: "Metallica", genero: "Heavy Metal"}
 ];
 //filtra los grupos por género obtenido aquellos que son de Heavy Metal.
+for (let grupo of grupos) {
+    if (grupo.genero === "Heavy Metal") {
+        console.log(grupo.nombre);
+    }
+}
+
 //Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
+let search = grupos.find(grupo => grupo.nombre === "Cold Play");
+console.log(search);
+
 //¿En qué posición del array se encuenta “Cold Play”?
+console.log("La posición es:");
 
-
+for (let i = 0; i < grupos.length; i++) {
+    if (grupos[i].nombre === "Cold Play") {
+        console.log(i);
+        i = grupos.length;
+    }
+}
