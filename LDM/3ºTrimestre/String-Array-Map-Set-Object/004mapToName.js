48cfae101b5b841cdfa5c34936bbc056ab592778
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-10 20:19:35 +0200

    Corrige el formato de los nombres en el array de objetos y simplifica la conversión a un array de nombres.

--- LDM/3ºTrimestre/String-Array-Map-Set-Object/004mapToName.js
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/004mapToName.js
@@ -1,15 +1,9 @@
 "use strict"
 
 // 004mapToName.js/.html: Tienes un array de objetos bands, cada uno tiene band.name. Escribe el código que lo convierta en un array de nombres. Por ejemplo:
-
-let acdc= { name: "ACDC", age: 25 };
-
-let metallica = { name: "Metallica", age: 30 };
-
-let guns= { name: "Guns and Roses", age: 28 };
-
+let acdc= { name: " ACDC", age: 25 };
+let metallica = { name: " Metallica", age: 30 };
+let guns= { name: " Guns and Roses", age: 28 };
 let bands = [ acdc, metallica, guns];
-
-let names = /* ... tu código */
-
+let names = [ acdc.name, metallica.name, guns.name];
 alert( names ); 
