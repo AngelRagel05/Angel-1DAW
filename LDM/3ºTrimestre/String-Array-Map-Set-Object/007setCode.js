87251a0894c576a79e07d9cb59fefe34442eaa96
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-11 09:22:03 +0200

    Añade ejemplos de uso de Set y sus métodos en el archivo 007setCode.js

--- LDM/3ºTrimestre/String-Array-Map-Set-Object/007setCode.js
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/007setCode.js
@@ -3,10 +3,26 @@
 // Crear un Set vacío llamado code.
 
 // Añadir al Set los siguientes elementos (respetando el orden): "JavaScript", "Python", "Java", "JavaScript" (duplicado), "C++".
-// Mostrar por consola el contenido inicial del Set y observar si se han insertado elementos duplicados.
-// Comprobar mediante el método has() si existen los elementos "Java" y "Ruby". Mostrar los resultados en consola.
-// Eliminar el elemento "C++" usando el método delete() y mostrar el contenido actualizado.
-// Iterar sobre los elementos del Set utilizando forEach() e imprimir cada elemento por consola.
-// Mostrar el número total de elementos actuales en el Set utilizando la propiedad size.
-// Eliminar todos los elementos del Set con el método clear() y mostrar el Set vacío y su nuevo tamaño.
+let code = new Set(["JavaScript", "Python", "Java", "JavaScript", "C++"]);
 
+// Mostrar por consola el contenido inicial del Set y observar si se han insertado elementos duplicados.
+console.log(code);
+
+// Comprobar mediante el método has() si existen los elementos "Java" y "Ruby". Mostrar los resultados en consola.
+console.log(code.has("Java"));
+console.log(code.has("Ruby"));
+
+// Eliminar el elemento "C++" usando el método delete() y mostrar el contenido actualizado.
+code.delete("C++");
+console.log(code);
+
+// Iterar sobre los elementos del Set utilizando forEach() e imprimir cada elemento por consola.
+code.forEach(cod => {
+    console.log(cod);
+});
+
+// Mostrar el número total de elementos actuales en el Set utilizando la propiedad size.
+console.log(code.size);
+
+// Eliminar todos los elementos del Set con el método clear() y mostrar el Set vacío y su nuevo tamaño.
+console.log(code.clear());
