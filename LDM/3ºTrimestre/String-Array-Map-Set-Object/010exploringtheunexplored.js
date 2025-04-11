15c56528a64980a1754224c399679163f5d64302
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-11 22:05:23 +0200

    Añade el archivo 010exploringtheunexplored.js con un ejemplo de uso de Map y sus operaciones básicas.

---
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/010exploringtheunexplored.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// Crea un objeto Map vacío llamado personajesUp.
+// Añade al Map los siguientes personajes y sus roles en la película:
+// Clave: "Carl Fredricksen", Valor: "Protagonista principal"
+// Clave: "Russell", Valor: "Explorador entusiasta"
+// Clave: "Dug", Valor: "Perro que habla"
+// Clave: "Kevin", Valor: "Ave tropical gigante"
+// Clave: "Charles Muntz", Valor: "Antagonista"
+// Muestra por consola el contenido inicial del Map.
+// Comprueba con has() si existen los personajes "Russell" y "Alpha" (el perro alfa). Muestra el resultado.
+// Obtén y muestra el rol del personaje "Dug" con get().
+// Actualiza el valor asociado a "Russell" a "Explorador leal y valiente" y muestra el nuevo valor.
+// Elimina al personaje "Charles Muntz" del Map usando delete() y muestra el contenido restante.
+// Itera sobre los personajes y sus descripciones con forEach() y muestra cada par formateado así:
+// "Carl Fredricksen: Protagonista principal"
+// Muestra cuántos personajes hay actualmente en el Map utilizando size.
+// Vacía el Map usando clear() y verifica que esté vacío mostrando su contenido y tamaño.
+
