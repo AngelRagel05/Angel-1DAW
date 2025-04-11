a1023dad42f78de0b1c9a676800b104a18f947de
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-11 22:27:24 +0200

    Elimina archivos HTML y JS obsoletos y añade un nuevo archivo HTML y JS para la gestión de cursos en línea.

---
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/012manageCourses.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Crea una clase que gestione cursos online. Cada curso tendrá estudiantes registrados. Usa objetos Set para asegurarte de que no se repitan 
+// estudiantes dentro de un curso. El ejercicio debe realizar las siguientes operaciones claramente separadas:
+
+// Crear una función para añadir estudiantes a un curso.
+// Crear una función para eliminar estudiantes de un curso.
+// Crear una función para unir estudiantes de dos cursos diferentes.
+// Crear una función para obtener estudiantes comunes entre dos cursos.
+// Crear una función que compruebe si un estudiante está en un curso específico.
+// Crear una función para vaciar completamente un curso
