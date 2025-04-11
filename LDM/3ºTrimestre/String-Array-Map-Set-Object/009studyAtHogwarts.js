e596a9e2216900bc1288a75a790551ee36664eaf
AngelRagel05 <jimenezragelangel@gmail.com>
2025-04-11 21:19:10 +0200

    Añade comentarios explicativos y ejemplos en el archivo 009studyAtHogwarts.js

--- LDM/3ºTrimestre/String-Array-Map-Set-Object/009studyAtHogwarts.js
+++ LDM/3ºTrimestre/String-Array-Map-Set-Object/009studyAtHogwarts.js
@@ -1,2 +1,27 @@
 "use strict"
 
+// Crea una función-objeto llamada hogwartsStudent que sea capaz de gestionar la siguiente información:
+ 
+//   nombre: "Hermione Granger",
+//   casa: "Gryffindor",
+//   mascota: "Crookshanks",
+//   asignaturas: ["Pociones", "Transformaciones", "Encantamientos"],
+//   año: 3,
+//   sangre: "mestiza",
+//   prefecto: true
+ 
+// 1. Muestra por consola el nombre completo del estudiante, la casa a la que pertenece y su mascota.
+// 2. Añade una nueva propiedad al objeto llamada patronus y asígnale el valor "Nutria".
+// 3. Agrega una nueva asignatura al array asignaturas, llamada "Defensa Contra las Artes Oscuras".
+// 4. Cambia el valor de la propiedad año a 4.
+// 5. Elimina la propiedad sangre del objeto.
+// 6. Crea una función printHogwartsStudent que reciba el objeto hogwartsStudent y muestre por consola un resumen como este:
+
+// Hermione Granger es una estudiante de Gryffindor en su cuarto año. Tiene una mascota llamada Crookshanks y su patronus es una Nutria. Sus
+// asignaturas son: Pociones, Transformaciones, Encantamientos, Defensa Contra las Artes Oscuras. Invoca la función printHogwartsStudent() 
+// con el objeto modificado. 
+
+// Reutiliza la función-objeto y muestra:
+
+// Ginny Weasley es una estudiante de Gryffindor en su cuarto año. Tiene una mascota llamada Arnold y su patronus es un Caballo. Sus asignaturas 
+// son: Encantamientos, Vuelo, Pociones, Defensa Contra las Artes Oscuras.
