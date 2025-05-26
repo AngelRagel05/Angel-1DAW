cba81e66f12644477ac376d51d8d60dc861bc158
AngelRagel05 <jimenezragelangel@gmail.com>
2025-05-26 12:53:21 +0200

    Actualizar validaciones en el formulario y mejorar la estructura del script de validación.

--- LDM/3ºTrimestre/Bootstrap5/Formularios/js/script.js
+++ LDM/3ºTrimestre/Bootstrap5/Formularios/js/script.js
@@ -1,61 +1,19 @@
-// <!-- Script de validación de Bootstrap con modificaciones de Olga, no tocar salvo los ids-->
-
-  (() => {
-
-   'use strict'
-
-   const forms = document.querySelectorAll('.needs-validation')
-
-   Array.from(forms).forEach(form => {
-
+(() => {
+  'use strict'
+  const forms = document.querySelectorAll('.needs-validation')
+  Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
-
-     // Validar hobbies manualmente
-
-     const checkboxes = form.querySelectorAll('.hobby:checked')
-
-     if (checkboxes.length === 0) {
-
-      event.preventDefault()
-
-      event.stopPropagation()
-
-      form.querySelector('.hobby').closest('fieldset') //id de los hobbies
-
-        .classList.add('is-invalid')
-
-     }
-
-     if (!form.checkValidity()) {
-
-      event.preventDefault()
-
-      event.stopPropagation()
-
-     }
-
-     // Validar coincidencia de contraseñas
-
-     const pass = form.querySelector('#pass').value // id contraseña 1
-
-     const pass2 = form.querySelector('#pass2').value // id contraseña 2
-
-     if (pass !== pass2) {
-
-      event.preventDefault()
-
-      event.stopPropagation()
-
-      form.querySelector('#pass2')
-
-        .classList.add('is-invalid')
-
-     }
-
-     form.classList.add('was-validated')
-
+      const checkedHobbies = form.querySelectorAll('input[name="Hobbies"]:checked')
+      if (checkedHobbies.length === 0) {
+        event.preventDefault(); event.stopPropagation();
+        form.querySelector('.invalid-feedback.d-block').classList.add('d-block')
+      }
+      if (!form.checkValidity()) { event.preventDefault(); event.stopPropagation(); }
+      const pass1 = form.querySelector('#password1').value
+      const pass2 = form.querySelector('#password2').value
+      if (pass1 !== pass2) { event.preventDefault(); event.stopPropagation(); form.querySelector('#password2').classList.add('is-invalid') }
+      form.classList.add('was-validated')
     }, false)
+  })
+})()
 
-   })
-
-  })()
